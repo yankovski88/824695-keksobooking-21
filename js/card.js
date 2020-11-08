060146fe975300b9c1b7d9a357309c9f2b8332dd
yura <boets003@mail.ru>
2020-11-09 01:31:19 +0300

    fix: card.js

--- js/card.js
+++ js/card.js
@@ -5,90 +5,84 @@
   // + прорисовка карты объявления
   const card = document.querySelector(`#card`).content.querySelector(`.map__card`); // нашел шаблон и подключился к нему
   const mapFiltersContainer = document.querySelector(`.map__filters-container`); // перед каким html элементов вставка
+  const map = document.querySelector(`.map`); // находим место в которое надо вставить карточку
 
   // создание карточки заполнение всех полей по шаблону
-  const createCard = function (obj) { // помещаем в функцию объект с которого возьмем данные
+  const createCard = function (objData) { // помещаем в функцию объект с которого возьмем данные
     const cardTemplate = card.cloneNode(true); // клонируем шаблон т.к. много объявлений и без клона не вставить в html
-    if (obj.offer) { // если есть описание offer
-      if (obj.offer.title) { // заходим в поле объекта заголовок
-        cardTemplate.querySelector(`.popup__title`).textContent = obj.offer.title; // переименовываем заголовок на заголовок с объекта
+      if (objData.offer.title) { // заходим в поле объекта заголовок
+        cardTemplate.querySelector(`.popup__title`).textContent = objData.offer.title; // переименовываем заголовок на заголовок с объекта
       } else {
         cardTemplate.querySelector(`.popup__title`).classList.add(`hidden`); // если нет заголовка, то скрываем эту строку карточки
       }
-      if (obj.offer.address[0] && obj.offer.address[1]) { // если есть текст адреса то вставляем
-        cardTemplate.querySelector(`.popup__text--address`).textContent = `${window.util.getFirstItemOfString(obj.offer.address)} Tōkyō-to, Chiyoda-ku, Ichibanchō, ${window.util.getLastItemOfString(obj.offer.address)}`;
+      if (objData.offer.address[0] && objData.offer.address[1]) { // если есть текст адреса то вставляем
+        cardTemplate.querySelector(`.popup__text--address`).textContent = `${objData.offer.address}`;
       } else {
         cardTemplate.querySelector(`.popup__text--address`).classList.add(`hidden`);
       }
-      if (obj.offer.price) {
-        cardTemplate.querySelector(`.popup__text--price`).textContent = obj.offer.price; // заполняем поле цены
+      if (objData.offer.price) {
+        cardTemplate.querySelector(`.popup__text--price`).textContent = objData.offer.price; // заполняем поле цены
       } else {
         cardTemplate.querySelector(`.popup__text--price`).classList.add(`hidden`);
       }
-      if (obj.offer.type) {
-        cardTemplate.querySelector(`.popup__type`).textContent = obj.offer.type;
+      if (objData.offer.type) {
+        cardTemplate.querySelector(`.popup__type`).textContent = objData.offer.type;
       } else {
         cardTemplate.querySelector(`.popup__type`).classList.add(`hidden`);
       }
-      if (obj.offer.rooms && obj.offer.guests) {
-        cardTemplate.querySelector(`.popup__text--capacity`).textContent = `${obj.offer.rooms} комнатa(ы) для ${obj.offer.guests} гостя(ей)`;
+      if (objData.offer.rooms && objData.offer.guests) {
+        cardTemplate.querySelector(`.popup__text--capacity`).textContent = `${objData.offer.rooms} комнатa(ы) для ${objData.offer.guests} гостя(ей)`;
       } else {
         cardTemplate.querySelector(`.popup__text--capacity`).classList.add(`hidden`);
       }
-      if (obj.offer.checkin && obj.offer.checkout) {
-        cardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${obj.offer.checkin}, выезд до ${obj.offer.checkout}`;
+      if (objData.offer.checkin && objData.offer.checkout) {
+        cardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${objData.offer.checkin}, выезд до ${objData.offer.checkout}`;
       } else {
         cardTemplate.querySelector(`.popup__text--time`).classList.add(`hidden`);
       }
-    } else {
-      obj.remove(); // иначе удаляем объект с объявлением. мысль если нет объекта, то он и не будет показываться
-    }
 
-    const cardTemplateLis = cardTemplate.querySelector(`.popup__features`).querySelectorAll(`li`); // находим все li теги с плюсами квартиры
-    const classNames = window.util.getArrClassNameHtml(cardTemplateLis); // получаем все названия классов массива li
-    const copyClassNames = classNames.slice(); // копируем полученный массив классов по приимуществам квартиры
-    const htmlOfferFeatures = window.util.getArrOfTextBeforeDash(copyClassNames); // получили массив плюсов квартиры
-    window.util.comparisonArrsAndAddClassNameHidden(obj.offer.features, htmlOfferFeatures, cardTemplateLis); // сравниваем 2 массива плюсов что есть с наличием по факту и добавляем название в html Элемент
 
-    if (obj.offer.description) {
-      cardTemplate.querySelector(`.popup__description`).textContent = `${obj.offer.description}`;
-    } else {
-      cardTemplate.querySelector(`.popup__description`).classList.add(`hidden`);
-    }
+      const cardTemplateLiTegs = cardTemplate.querySelector(`.popup__features`).querySelectorAll(`li`); // находим все li теги с плюсами квартиры
+      const classNames = window.util.getArrClassNameHtml(cardTemplateLiTegs); // получаем все названия классов массива li
+      const copyClassNames = classNames.slice(); // копируем полученный массив классов по приимуществам квартиры
+      const htmlOfferFeatures = window.util.getArrOfTextBeforeDash(copyClassNames); // получили массив плюсов квартиры
+      window.util.comparisonArrsAndAddClassNameHidden(objData.offer.features, htmlOfferFeatures, cardTemplateLiTegs); // сравниваем 2 массива плюсов что есть с наличием по факту и добавляем название в html Элемент
 
-    const popupPhoto = cardTemplate.querySelector(`.popup__photos`); // находим в карточке обертку тега Img в которую будем все вставлять
-    const popupPhotoImg = cardTemplate.querySelector(`.popup__photos`).querySelector(`img`); // находим сам тег img
-
-    if (obj.offer.photos) { // если фото есть
-      for (let i = 0; i < obj.offer.photos.length; i++) { // перебираем фото
-        const popupPhotoImgClone = popupPhotoImg.cloneNode(); // клонируем тег img для вставки фото
-        popupPhotoImgClone.src = obj.offer.photos[i]; // в тег img вставляем ссылку
-        popupPhotoImgClone.textContent = `clone`; // между тегом img пишем clone
-        popupPhoto.appendChild(popupPhotoImgClone); // добавляем в обертку все теги img
+      if (objData.offer.description) {
+        cardTemplate.querySelector(`.popup__description`).textContent = `${objData.offer.description}`;
+      } else {
+        cardTemplate.querySelector(`.popup__description`).classList.add(`hidden`);
       }
-      popupPhoto.removeChild(popupPhotoImg); // удаляем  шаблонное фото
-    } else {
-      popupPhoto.classList.add(`hidden`); // если нет фото то скрываем поле
-    }
 
-    if (obj.author.avatar) {
-      cardTemplate.querySelector(`.popup__avatar`).src = obj.author.avatar; // добавляем аватар
-    } else {
-      cardTemplate.querySelector(`.popup__avatar`).classList.add(`hidden`);
-    }
+      const popupPhoto = cardTemplate.querySelector(`.popup__photos`); // находим в карточке обертку тега Img в которую будем все вставлять
+      const popupPhotoImg = popupPhoto.querySelector(`img`); // находим сам тег img
+
+      if (objData.offer.photos) { // если фото есть
+        for (let i = 0; i < objData.offer.photos.length; i++) { // перебираем фото
+          const popupPhotoImgClone = popupPhotoImg.cloneNode(); // клонируем тег img для вставки фото
+          popupPhotoImgClone.src = objData.offer.photos[i]; // в тег img вставляем ссылку
+          popupPhotoImgClone.textContent = `clone`; // между тегом img пишем clone
+          popupPhoto.appendChild(popupPhotoImgClone); // добавляем в обертку все теги img
+        }
+        popupPhoto.removeChild(popupPhotoImg); // удаляем  шаблонное фото
+      } else {
+        popupPhoto.classList.add(`hidden`); // если нет фото то скрываем поле
+      }
+
+      if (objData.author.avatar) {
+        cardTemplate.querySelector(`.popup__avatar`).src = objData.author.avatar; // добавляем аватар
+      } else {
+        cardTemplate.querySelector(`.popup__avatar`).classList.add(`hidden`);
+      }
+    // }
     return cardTemplate; // возвращаем полностью заполненую карточку по шаблону
+
   };
 
-  const map = document.querySelector(`.map`); // находим место в которое надо вставить карточку
 
   // функция по вставке элемента перед другим элементом
   const renderCard = function (fragmentCardItem, mapFiltersContainerItem) {
-    map.insertBefore(fragmentCardItem, mapFiltersContainerItem);
-
-  // map это родитель для нового элемента
-  // insertBefore(newElement, referenceElement)
-  // newElement это элемент для вставки
-  // referenceElement элемент перед которым будет вставлен newElement
+    map.insertBefore(fragmentCardItem, mapFiltersContainerItem); // map это родитель для нового элемента, fragmentCardItem это элемент для вставки, mapFiltersContainerItem перед этим элементом вставится элемент
   };
 
   window.card = {
--- js/pin.js
+++ js/pin.js
@@ -7,75 +7,79 @@
   const pin = document.querySelector(`#pin`).content.querySelector(`.map__pin`); // нашли шаблон по pin
 
   const createPin = function (objData) { // по этому макету создается метка
-    const pinTemplate = pin.cloneNode(true); // создаем клоны метки
-    const img = pinTemplate.querySelector(`img`); // создал константу чтобы 2 раза не искать
-    img.src = objData.author.avatar; // вставили ссылку на аватар
-    img.alt = objData.offer.title; // вставили заголовок
-    pinTemplate.style.left = `${objData.location.x}px`; // вставили координаты
-    pinTemplate.style.top = `${objData.location.y}px`;
+    if (objData.offer) { // если есть описание offer,  то рисуем метки.
 
-    pinTemplate.addEventListener(`click`, (evt) => { // отслеживаем клик по каждой созданой метке
+      const pinTemplate = pin.cloneNode(true); // создаем клоны метки
+      const img = pinTemplate.querySelector(`img`); // создал константу чтобы 2 раза не искать
+      img.src = objData.author.avatar; // вставили ссылку на аватар
+      img.alt = objData.offer.title; // вставили заголовок
+      pinTemplate.style.left = `${objData.location.x}px`; // вставили координаты
+      pinTemplate.style.top = `${objData.location.y}px`;
 
-      let target = evt.target; // цель по которой был клик
-      if (target.tagName === `IMG`) { // если таргет был с тегом IMG
-        target = target.parentNode; // то переопределяем таргет на его родителя, с помощью target.parentNode
-      }
+      pinTemplate.addEventListener(`click`, (evt) => { // отслеживаем клик по каждой созданой метке
 
-      // код установки активной метки
-      const mapItemPins = document.querySelectorAll(`.map__pin`); // находим все метки после рендера
-      // функция удаления всех активных меток
-      const removeMapPinActive = function () {
-        mapItemPins.forEach((item) => { // перебираем все метки
-          if (item.classList.contains(`map__pin--active`)) { // если находим активную метку
-            item.classList.remove(`map__pin--active`); // то удаляем в ней класс активности
-          }
-        });
-      };
-      // функция по установке активной метки
-      const setMapPinActive = function () {
-        target.classList.add(`map__pin--active`); // добавление к метке класа актив
-      };
-      removeMapPinActive(); // удаление активной метки
-      setMapPinActive(); // установка активной метки
-
-
-      // код по открытию карточки квартир
-      if ((target.classList.contains(`map__pin`)) && (!target.classList.contains(`map__pin--main`))) { // делаем проверку или это не главная метка
-        if (window.card.map.querySelector(`.map__card`)) { // если наша карточка находится в map это означает, что она открыта
-          window.card.map.removeChild(window.card.map.querySelector(`.map__card`)); // и удаляем ее
-          removeMapPinActive();
-          setMapPinActive();
-          window.card.renderCard(window.card.createCard(objData), window.card.mapFiltersContainer); // если был клик по метке, то он записывается в target и создаем карточку с того же объекта что и метку
-        } else {
-          removeMapPinActive();
-          setMapPinActive();
-          window.card.renderCard(window.card.createCard(objData), window.card.mapFiltersContainer); // если был клик по метке, то он записывается в target и создаем карточку с того же объекта что и метку
+        let target = evt.target; // цель по которой был клик
+        if (target.tagName === `IMG`) { // если таргет был с тегом IMG
+          target = target.parentNode; // то переопределяем таргет на его родителя, с помощью target.parentNode
         }
-      }
 
-
-      const popupClose = document.querySelector(`.popup__close`);
-      const mapCard = window.card.map.querySelector(`.map__card`);
-      // удаление карточки
-      const removeChildMapCard = function () {
-        window.card.map.removeChild(mapCard);
-      };
-      const onPopupCloseClick = function () {
-        removeChildMapCard(); // удаление карточки
+        // код установки активной метки
+        const mapItemPins = document.querySelectorAll(`.map__pin`); // находим все метки после рендера
+        // функция удаления всех активных меток
+        const removeMapPinActive = function () {
+          mapItemPins.forEach((item) => { // перебираем все метки
+            if (item.classList.contains(`map__pin--active`)) { // если находим активную метку
+              item.classList.remove(`map__pin--active`); // то удаляем в ней класс активности
+            }
+          });
+        };
+        // функция по установке активной метки
+        const setMapPinActive = function () {
+          target.classList.add(`map__pin--active`); // добавление к метке класа актив
+        };
         removeMapPinActive(); // удаление активной метки
-      };
-      popupClose.addEventListener(`click`, onPopupCloseClick);
+        setMapPinActive(); // установка активной метки
 
-      const onPopupCloseEnterPress = function () {
-        if (evt.target.code === window.card.KEY_CODE_ENTER) {
+
+        // код по открытию карточки квартир
+        if ((target.classList.contains(`map__pin`)) && (!target.classList.contains(`map__pin--main`))) { // делаем проверку или это не главная метка
+          if (window.card.map.querySelector(`.map__card`)) { // если наша карточка находится в map это означает, что она открыта
+            window.card.map.removeChild(window.card.map.querySelector(`.map__card`)); // и удаляем ее
+            removeMapPinActive();
+            setMapPinActive();
+            window.card.renderCard(window.card.createCard(objData), window.card.mapFiltersContainer); // если был клик по метке, то он записывается в target и создаем карточку с того же объекта что и метку
+          } else {
+            removeMapPinActive();
+            setMapPinActive();
+            window.card.renderCard(window.card.createCard(objData), window.card.mapFiltersContainer); // если был клик по метке, то он записывается в target и создаем карточку с того же объекта что и метку
+          }
+        }
+
+
+        const popupClose = document.querySelector(`.popup__close`);
+        const mapCard = window.card.map.querySelector(`.map__card`);
+        // удаление карточки
+        const removeChildMapCard = function () {
+          window.card.map.removeChild(mapCard);
+        };
+        const onPopupCloseClick = function () {
           removeChildMapCard(); // удаление карточки
           removeMapPinActive(); // удаление активной метки
-        }
-      };
-      popupClose.addEventListener(`keydown`, onPopupCloseEnterPress); // думаю эти колбеки можно не удалять т.к.
-      // если удалять то сробатывает область видимости
-    });
-    return pinTemplate; // вернули метку
+        };
+        popupClose.addEventListener(`click`, onPopupCloseClick);
+
+        const onPopupCloseEnterPress = function () {
+          if (evt.target.code === window.card.KEY_CODE_ENTER) {
+            removeChildMapCard(); // удаление карточки
+            removeMapPinActive(); // удаление активной метки
+          }
+        };
+        popupClose.addEventListener(`keydown`, onPopupCloseEnterPress); // думаю эти колбеки можно не удалять т.к.
+        // если удалять то сробатывает область видимости
+      });
+
+      return pinTemplate; // вернули метку
+    }
   };
 
   window.pin = {
