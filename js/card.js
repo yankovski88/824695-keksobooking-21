1fbb338fb783fa2a3f9bf6c9276884b2660970e8
yura <boets003@mail.ru>
2020-11-09 07:24:28 +0300

    fix: card.js full

--- js/card.js
+++ js/card.js
@@ -7,44 +7,50 @@
   const mapFiltersContainer = document.querySelector(`.map__filters-container`); // перед каким html элементов вставка
   const map = document.querySelector(`.map`); // находим место в которое надо вставить карточку
 
+  const addClassHidden = function (elementHtml) {
+    elementHtml.classList.add(`hidden`);
+  };
   // создание карточки заполнение всех полей по шаблону
   const createCard = function (objData) { // помещаем в функцию объект с которого возьмем данные
     const cardTemplate = card.cloneNode(true); // клонируем шаблон т.к. много объявлений и без клона не вставить в html
     if (objData.offer.title) { // заходим в поле объекта заголовок
       cardTemplate.querySelector(`.popup__title`).textContent = objData.offer.title; // переименовываем заголовок на заголовок с объекта
     } else {
-      cardTemplate.querySelector(`.popup__title`).classList.add(`hidden`); // если нет заголовка, то скрываем эту строку карточки
+      addClassHidden(cardTemplate.querySelector(`.popup__title`));
     }
     if (objData.offer.address[0] && objData.offer.address[1]) { // если есть текст адреса то вставляем
       cardTemplate.querySelector(`.popup__text--address`).textContent = `${objData.offer.address}`;
     } else {
-      cardTemplate.querySelector(`.popup__text--address`).classList.add(`hidden`);
+      addClassHidden(cardTemplate.querySelector(`.popup__text--address`));
     }
     if (objData.offer.price) {
       cardTemplate.querySelector(`.popup__text--price`).textContent = objData.offer.price; // заполняем поле цены
     } else {
-      cardTemplate.querySelector(`.popup__text--price`).classList.add(`hidden`);
+      addClassHidden(cardTemplate.querySelector(`.popup__text--price`));
     }
     if (objData.offer.type) {
       cardTemplate.querySelector(`.popup__type`).textContent = objData.offer.type;
     } else {
-      cardTemplate.querySelector(`.popup__type`).classList.add(`hidden`);
+      addClassHidden(cardTemplate.querySelector(`.popup__type`));
     }
     if (objData.offer.rooms && objData.offer.guests) {
       cardTemplate.querySelector(`.popup__text--capacity`).textContent = `${objData.offer.rooms} комнатa(ы) для ${objData.offer.guests} гостя(ей)`;
     } else {
-      cardTemplate.querySelector(`.popup__text--capacity`).classList.add(`hidden`);
+      addClassHidden(cardTemplate.querySelector(`.popup__text--capacity`));
     }
     if (objData.offer.checkin && objData.offer.checkout) {
       cardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${objData.offer.checkin}, выезд до ${objData.offer.checkout}`;
     } else {
-      cardTemplate.querySelector(`.popup__text--time`).classList.add(`hidden`);
+      addClassHidden(cardTemplate.querySelector(`.popup__text--time`));
     }
 
 
     const cardTemplateLiTegs = cardTemplate.querySelector(`.popup__features`).querySelectorAll(`li`); // находим все li теги с плюсами квартиры
     const classNames = window.util.getArrClassNameHtml(cardTemplateLiTegs); // получаем все названия классов массива li
-    const copyClassNames = classNames.slice(); // копируем полученный массив классов по приимуществам квартиры
+    const copyClassNames = classNames.map(function (item) {
+      return item;
+    }); // копируем полученный массив классов по приимуществам квартиры
+
     const htmlOfferFeatures = window.util.getArrOfTextBeforeDash(copyClassNames); // получили массив плюсов квартиры
     window.util.comparisonArrsAndAddClassNameHidden(objData.offer.features, htmlOfferFeatures, cardTemplateLiTegs); // сравниваем 2 массива плюсов что есть с наличием по факту и добавляем название в html Элемент
 
