ca620f51f98c5ec5a6acb6b88ab945b2d3bd8387
yura <boets003@mail.ru>
2020-11-09 04:14:23 +0300

    fix: form.js full

--- js/form.js
+++ js/form.js
@@ -7,13 +7,23 @@
   const KEY_CODE_ENTER = 13;
   const MIN_LENGTH = 30;
   const MAX_PRICE = 1000000;
-  // переменная с указанием информации что нет таких комант с гостями
-  //   const MESSAGE_ROOMS_ERROR = `
-  // 1 комната — «для 1 гостя»;
-  // 2 комнаты — «для 2 гостей» или «для 1 гостя»;
-  // 3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;
-  // 100 комнат — «не для гостей».`;
 
+  const capacity = document.querySelector(`#capacity`); // нашли id формы по гостям
+  const capacityOptions = capacity.querySelectorAll(`option`); // выбрали у нее все всплывающие пункты
+  const СountGuests = [[0, 1, 3], [0, 3], [3], [0, 1, 2]]; // это перечисление дляустановки определенным полям гостей disabled
+
+  const avatar = document.querySelector(`#avatar`);
+  const image = document.querySelector(`#images`);
+  const timein = document.querySelector(`#timein`);
+  const timeinOptions = timein.querySelectorAll(`option`);
+  const timeout = document.querySelector(`#timeout`);
+  const timeoutOptions = timeout.querySelectorAll(`option`);
+  const mapPins = document.querySelector(`.map__pins`);
+  const success = document.querySelector(`#success`).content.querySelector(`.success`); // нашел шаблон для вставки
+  const successTemplate = success.cloneNode(true); // обязательно клонируем шаблон, без клона не вставится в html
+  const error = document.querySelector(`#error`).content.querySelector(`.error`); // нашел шаблон
+  const nodeError = error.cloneNode(true); // делаем клон ноду шаблона без нее не вставим
+  let errorButton; // вынес кнопку в глобальную видимость т.к. нужно ее найти только после добавленя ее в DOM
 
   const START_ADDRESS_X = Math.round(parseInt(window.movePin.MAP_PIN_MAIN_LEFT, 10) + window.movePin.mapPinMain.offsetWidth / 2);
   const START_ADDRESS_Y = Math.round(parseInt(window.movePin.MAP_PIN_MAIN_TOP, 10) + window.movePin.mainPinHeight);
@@ -64,8 +74,6 @@
     window.main.mapPinMain.addEventListener(`mousedown`, window.main.onMapPinMainMousedown);
   };
 
-  const capacity = document.querySelector(`#capacity`); // нашли id формы по гостям
-  const capacityOptions = capacity.querySelectorAll(`option`); // выбрали у нее все всплывающие пункты
 
   // удаляем атрибут disabled в полученом массиве option
   const removeToArrDisabled = function (arr) {
@@ -74,7 +82,6 @@
     });
   };
   removeToArrDisabled(capacityOptions);
-  const СountGuests = [[0, 1, 3], [0, 3], [3], [0, 1, 2]]; // это перечисление дляустановки определенным полям гостей disabled
 
   // установка поле гостя по умолчанию
   const setGuestsDefault = function () {
@@ -84,51 +91,36 @@
     capacityOptions[2].selected = true; // покажется активный выриант колличества гостей
   };
   setGuestsDefault();
-
+  // 0      CountGuests      capacityOptions   2
+  const setDisabledGuest = function (indexСountGuests, countGuests, guestOptions, indexSelectedGuestOptions) {
+    for (let i = 0; i < countGuests[indexСountGuests].length; i++) { // перебираем массив для 1 комнаты
+      guestOptions[countGuests[indexСountGuests][i]].setAttribute(`disabled`, `true`); // устанавливаем всеим полям гостей disabled
+    }
+    guestOptions[indexSelectedGuestOptions].selected = true; // покажется активный выриант колличества гостей
+  };
   const checkRoomAndGuest = function () {
     const roomNumber = document.querySelector(`#room_number`); // нашел поле комнат
 
-    // идет работа по функция компанты и гости
-    // let room = roomNumber.value; // значение поля рум сохранил в room
-
+    // идет работа по функция комнаты и гости
     const onRoomNumberChange = function () {
       const ROOM_ONE = `1`;
       const ROOM_TWO = `2`;
       const ROOM_THREE = `3`;
 
       const roomValue = roomNumber.value; // нашли значение выбраного поля пользователем
-      // room = roomValue; // перегнали значение в другую переменную, для чего не знаю
       if (roomValue === ROOM_ONE) { // если значение 1 то это 1 комната выбрана
         window.main.removeAddDisabled(capacityOptions); // функция удаляет у всех полей disabled
-
-        for (let i = 0; i < СountGuests[0].length; i++) { // перебираем массив для 1 комнаты
-          capacityOptions[СountGuests[0][i]].setAttribute(`disabled`, `true`); // устанавливаем всеим полям гостей disabled
-        }
-        capacityOptions[2].selected = true; // покажется активный выриант колличества гостей
-      }
-      if (roomValue === ROOM_TWO) { // если значение 1 то это 1 комната выбрана
+        setDisabledGuest(0, СountGuests, capacityOptions, 2);
+      } else if (roomValue === ROOM_TWO) { // если значение 1 то это 1 комната выбрана
         window.main.removeAddDisabled(capacityOptions);
-        for (let i = 0; i < СountGuests[1].length; i++) { // перебираем массив для 1 комнаты
-
-          capacityOptions[СountGuests[1][i]].setAttribute(`disabled`, `true`); // устанавливаем всеим полям гостей disabled
-        }
-        capacityOptions[1].selected = true; // покажется активный выриант колличества гостей
-      }
-      if (roomValue === ROOM_THREE) { // если значение 1 то это 1 комната выбрана
+        setDisabledGuest(1, СountGuests, capacityOptions, 1);
+      } else if (roomValue === ROOM_THREE) { // если значение 1 то это 1 комната выбрана
         window.main.removeAddDisabled(capacityOptions);
-
-        for (let i = 0; i < СountGuests[2].length; i++) { // перебираем массив для 1 комнаты
-          capacityOptions[СountGuests[2][i]].setAttribute(`disabled`, `true`); // устанавливаем всеим полям гостей disabled
-        }
-        capacityOptions[0].selected = true; // покажется активный выриант колличества гостей
+        setDisabledGuest(2, СountGuests, capacityOptions, 0);
       }
       if (roomValue > СountGuests.length - 1) { // если значение 1 то это 1 комната выбрана
         window.main.removeAddDisabled(capacityOptions);
-
-        for (let i = 0; i < СountGuests[3].length; i++) { // перебираем массив для 1 комнаты
-          capacityOptions[СountGuests[3][i]].setAttribute(`disabled`, `true`); // устанавливаем всеим полям гостей disabled
-        }
-        capacityOptions[3].selected = true; // покажется активный выриант колличества гостей
+        setDisabledGuest(3, СountGuests, capacityOptions, 3);
       }
     };
 
@@ -141,11 +133,11 @@
   price.setAttribute(`max`, MAX_PRICE);
 
   const getArrValueFromHtml = function (arrHtml) {
-    const itemValue = [];
+    const itemValues = [];
     arrHtml.forEach((item) => {
-      itemValue.push(item.value);
+      itemValues.push(item.value);
     });
-    return itemValue;
+    return itemValues;
   };
 
 
@@ -170,39 +162,35 @@
   price.setAttribute(`type`, `number`);
   price.setAttribute(`max`, `${MAX_PRICE}`);
 
-  const avatar = document.querySelector(`#avatar`);
+
   avatar.setAttribute(`accept`, `image/*`);
 
-  const images = document.querySelector(`#images`);
-  images.setAttribute(`accept`, `image/*`);
+  image.setAttribute(`accept`, `image/*`);
 
-  const timein = document.querySelector(`#timein`);
-  const timeinOptions = timein.querySelectorAll(`option`);
-  const timeout = document.querySelector(`#timeout`);
-  const timeoutOptions = timeout.querySelectorAll(`option`);
 
   const setTimeinAndTimeout = function () {
     const onTimeinChange = function () {
-      timeinOptions.forEach((item) => {
-        if (item.value === timein.value) {
-          timeout.value = item.value;
+      for (let i = 0; i < timeinOptions.length; i++) {
+        if (timeinOptions[i].value === timein.value) {
+          timeout.value = timeinOptions[i].value;
+          break;
         }
-      });
+      }
     };
 
     timein.addEventListener(`change`, onTimeinChange);
 
     const onTimeoutChange = function () {
-      timeoutOptions.forEach((item) => {
-        if (item.value === timeout.value) {
-          timein.value = item.value;
+      for (let i = 0; i < timeoutOptions.length; i++) {
+        if (timeoutOptions[i].value === timeout.value) {
+          timein.value = timeoutOptions[i].value;
+          break;
         }
-      });
+      }
+
     };
     timeout.addEventListener(`change`, onTimeoutChange);
   };
-
-  const mapPins = document.querySelector(`.map__pins`);
 
   // после того как форма отправится любым способом, то вызовитесь следующие колбеки
   const form = document.querySelector(`.ad-form`);
@@ -211,8 +199,7 @@
     window.backend.save(new FormData(form), onSuccess, showErrorSave);
   });
 
-  const success = document.querySelector(`#success`).content.querySelector(`.success`); // нашел шаблон для вставки
-  const successTemplate = success.cloneNode(true); // обязательно клонируем шаблон, без клона не вставится в html
+
   const onSuccess = function () { // обработчик отправки успешной формы
     mapPins.appendChild(successTemplate); // Добавил нод в Html
     document.addEventListener(`keydown`, onSuccessPressEsc); // добавивл обработчик по ссылке
@@ -231,24 +218,22 @@
     });
   };
 
+  // функция по удалению успешного окна отправки
+  const removeShowSuccess = function () {
+    successTemplate.remove();
+    document.removeEventListener(`keydown`, onSuccessPressEsc);
+    document.removeEventListener(`click`, onSuccessClick);
+  };
   // Добавляю удаление сообщения об успешной отправке через ESC
   const onSuccessPressEsc = function (evt) {
     if (evt.keyCode === KEY_CODE_ESC) {
-      successTemplate.remove();
-      document.removeEventListener(`keydown`, onSuccessPressEsc);
-      document.removeEventListener(`click`, onSuccessClick);
-
+      removeShowSuccess(); // функция удаления успешного окна отправки
     }
   };
   const onSuccessClick = function () {
-    successTemplate.remove();
-    document.removeEventListener(`click`, onSuccessClick);
-    document.removeEventListener(`keydown`, onSuccessPressEsc);
-
+    removeShowSuccess(); // функция удаления успешного окна отправки
   };
-  const error = document.querySelector(`#error`).content.querySelector(`.error`); // нашел шаблон
-  const nodeError = error.cloneNode(true); // делаем клон ноду шаблона без нее не вставим
-  let errorButton; // вынес кнопку в глобальную видимость т.к. нужно ее найти только после добавленя ее в DOM
+
   // сделал функцю по обработке ошибки
   const showErrorSave = function () {
     const main = document.querySelector(`main`); // нашел куда вставляем
@@ -261,28 +246,24 @@
 
   };
 
-  // создаю колбек функцию по ссылке для удаления окна с ошибкой
-  const onAroundOfErrorButtonClick = function () { // пишу функцию по клику на кнопку закрытия
+  const removeShowError = function () {
     nodeError.remove(); // удаляем наш node Error
     document.removeEventListener(`click`, onAroundOfErrorButtonClick);
     document.removeEventListener(`keydown`, onErrorButtonPressEsc);
     errorButton.removeEventListener(`click`, onErrorButtonClick);
+  };
+  // создаю колбек функцию по ссылке для удаления окна с ошибкой
+  const onAroundOfErrorButtonClick = function () { // пишу функцию по клику на кнопку закрытия
+    removeShowError();
   };
 
   const onErrorButtonPressEsc = function (evt) {
     if (evt.keyCode === KEY_CODE_ESC) {
-      nodeError.remove(); // удаляем наш node Error
-      document.removeEventListener(`keydown`, onErrorButtonPressEsc);
-      document.removeEventListener(`click`, onAroundOfErrorButtonClick);
-      errorButton.removeEventListener(`click`, onErrorButtonClick);
-
+      removeShowError();
     }
   };
   const onErrorButtonClick = function () {
-    nodeError.remove(); // удаляем наш node Error
-    errorButton.removeEventListener(`click`, onErrorButtonClick);
-    document.removeEventListener(`keydown`, onErrorButtonPressEsc);
-    document.removeEventListener(`click`, onAroundOfErrorButtonClick);
+    removeShowError();
   };
 
   // сделал обработчик клика на очистку формы
