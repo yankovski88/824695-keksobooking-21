cd4bb5ccb306c9ad02ed0864f7c3bc6c4ecd021d
yura <boets003@mail.ru>
2020-11-10 20:27:58 +0300

    workin Parking

--- js/filter.js
+++ js/filter.js
@@ -49,17 +49,17 @@ const renderPin = function () { // отрисовать метки
 const renderNewPin = function (newPins) {
   delPin();
   let countPin = 0;
-  if (newPins.length < window.pin.MAX_PIN){
-    countPin = newPins.length
+  if (newPins.length < window.pin.MAX_PIN) {
+    countPin = newPins.length;
   } else {
-    countPin = window.pin.MAX_PIN
+    countPin = window.pin.MAX_PIN;
   }
 
-    for (let i = 0; i < countPin; i++) { // перебрать все данные которые получены и перенесены в переменную
-      const fragmentPin = window.pin.createPin(newPins[i]); // создаем метку через функцию выше
-      fragment.appendChild(fragmentPin); // в созданный фрагмент вставляем все наши метки
-      renderPin(); // прорисовываем метки
-    }
+  for (let i = 0; i < countPin; i++) { // перебрать все данные которые получены и перенесены в переменную
+    const fragmentPin = window.pin.createPin(newPins[i]); // создаем метку через функцию выше
+    fragment.appendChild(fragmentPin); // в созданный фрагмент вставляем все наши метки
+    renderPin(); // прорисовываем метки
+  }
 };
 
 const filterPin = function (arr) {
